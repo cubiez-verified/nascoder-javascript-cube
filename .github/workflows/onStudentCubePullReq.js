120d0fac72d2558307f44748fe202ab1612e481b
Nasrin Tohidi <nasrin@kidocode.com>
2019-12-17 17:53:07 +0800

    Update onStudentCubePullReq.js

--- .github/workflows/onStudentCubePullReq.js
+++ .github/workflows/onStudentCubePullReq.js
@@ -1,1 +1,8 @@
-console.log("hi")
+const axios = require("axios")
+
+const a = async (gitToken) => {
+  await axios.post("https://88a4fa7d.ngrok.io/api/update-cube", {gitToken})
+  return true
+}
+
+a(process.argv[2]).then(r => {console.log(r)})
